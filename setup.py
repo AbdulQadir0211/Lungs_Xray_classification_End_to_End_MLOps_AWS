d830e07410d2db05027b4d22dcbf5ab9a54b28d0
AbdulQadir0211 <abdulkadir9929@gmail.com>
2024-07-17 02:11:16 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements(filepath:str)->List[str]:
+    requirements = []
+    with open(filepath) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name = "Xray",
+    version="0.1",
+    author="Abdul Qadir",
+    author_email= "abdulkadir9929@gmail.com",
+    install_requires = get_requirements(),
+    packages = find_packages()
+)
